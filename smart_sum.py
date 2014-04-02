8e9d9f86b999c1b086214ec992e5822b3dd4e527
Stephen Mann <thanthese@gmail.com>
2014-04-02 16:27:47 -0500

    removed parse() func

--- smart_sum.py
+++ smart_sum.py
@@ -3,16 +3,10 @@
 import decimal
 import sys
 
+
 def smart_sum(s):
     num_chars = filter(lambda c: c in "\t\n -.0123456789", s)
-    return str(sum(map(parse, num_chars.split())))
-
-
-def parse(s):
-    try:
-        return int(s)
-    except ValueError:
-        return decimal.Decimal(s)
+    return str(sum(map(decimal.Decimal, num_chars.split())))
 
 
 def main():
